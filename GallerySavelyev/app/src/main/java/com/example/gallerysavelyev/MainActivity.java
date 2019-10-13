1e89b4274fa7642797640af090c795bc57e0f467
Andrey Savelyev <fess_87@mail.ru>
2019-10-13 13:22:26 +0300

    New example Gallery

---
+++ GallerySavelyev/app/src/main/java/com/example/gallerysavelyev/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.gallerysavelyev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Random random = new Random();
+        final TextView text = (TextView) findViewById(R.id.outputTxt);
+        Button forwardBotton = (Button) findViewById(R.id.forwardButton);
+        forwardBotton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        text.setText("http://myfile.org/"+String.valueOf(random.nextInt(100)));
+        Button backButton = (Button)findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
