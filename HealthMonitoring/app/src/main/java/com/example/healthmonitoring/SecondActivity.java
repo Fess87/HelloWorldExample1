0acc3be9977fc79dcafa6afe035bbcd6846d8039
Andrey Savelyev <fess_87@mail.ru>
2019-10-15 12:06:55 +0300

    first work version app

--- HealthMonitoring/app/src/main/java/com/example/healthmonitoring/SecondActivity.java
+++ HealthMonitoring/app/src/main/java/com/example/healthmonitoring/SecondActivity.java
@@ -1,14 +1,49 @@
 package com.example.healthmonitoring;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
+    List<IndividualIndicators> list = new ArrayList<>();
+    protected static final String TAG = "MyApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        savePressure(list);
+    }
+
+    protected void savePressure(final List<IndividualIndicators> list){
+        Button button = findViewById(R.id.buttonSavePressure);
+        button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                EditText highPressure = findViewById(R.id.editHighPressure);
+                EditText lowPressure = findViewById(R.id.editLowPressure);
+                EditText pulse = findViewById(R.id.editPulse);
+                CheckBox tachycardia = findViewById(R.id.tachycardia);
+                Log.i(TAG, "Сохранение параметров давления");
+                try {
+                    list.add(new IndividualIndicators(Integer.valueOf(highPressure.getText().toString()), Integer.valueOf(lowPressure.getText().toString()), Integer.valueOf(pulse.getText().toString()), tachycardia.isChecked()));
+                } catch (Exception ex){
+                    Toast.makeText(SecondActivity.this,ex.getMessage(),Toast.LENGTH_LONG).show();
+                }
+                }
+        });
     }
 }
--- HealthMonitoring/app/src/main/java/com/example/healthmonitoring/ThirdActivity.java
+++ HealthMonitoring/app/src/main/java/com/example/healthmonitoring/ThirdActivity.java
@@ -2,13 +2,44 @@ package com.example.healthmonitoring;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ThirdActivity extends AppCompatActivity {
+    List<VitalIndicators> list = new ArrayList<>();
+    protected static final String TAG = "MyApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        saveHealth(list);
+    }
+
+    protected void saveHealth(final List<VitalIndicators> list){
+
+        Button button = findViewById(R.id.buttonSaveHealth);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText weight = findViewById(R.id.editWeight);
+                EditText steps = findViewById(R.id.editSteps);
+                Log.i(TAG, "Сохранение жизненных показателей");
+                try{
+                list.add(new VitalIndicators(Double.parseDouble(weight.getText().toString()), Integer.parseInt(steps.getText().toString())));
+                }catch (Exception ex){
+                    Toast.makeText(ThirdActivity.this,ex.getMessage(),Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
