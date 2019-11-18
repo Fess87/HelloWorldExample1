83066d8b38c3ac88eb421d543c771523b7d49c52
Andrey Savelyev <fess_87@mail.ru>
2019-11-18 21:20:54 +0300

    update app with listView and action.

--- ListViewActivity/app/src/main/java/com/example/listviewactivity/MainActivity.java
+++ ListViewActivity/app/src/main/java/com/example/listviewactivity/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.listviewactivity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -15,39 +19,87 @@ import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences TxtSharedPref;
+    private static String LARGE_TEXT = "large_text";
+    private List<Map<String, String>> contentList;
+    private BaseAdapter listContentAdapter;
+    private SwipeRefreshLayout swipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        saveInSharedPreferences();
 
-        ListView list = findViewById(R.id.list);
+        initList();
+        initSwipeToRefresh();
 
-        List<Map<String, String>> values = prepareContent();
-
-        BaseAdapter listContentAdapter = createAdapter(values);
-
-        list.setAdapter(listContentAdapter);
     }
 
     @NonNull
-    private SimpleAdapter createAdapter(List<Map<String, String>>  content) {
-        return new SimpleAdapter(this, content, R.layout.list_with_heading, new String[]{"text","length"}, new int[]{R.id.textViewWithTxt, R.id.textViewWithNumberOfChars});
+    private SimpleAdapter createAdapter(String[] stringsTxt) {
+        contentList = new ArrayList<>();
+        prepareAdapterContent(stringsTxt);
+        return new SimpleAdapter(this, contentList, R.layout.list_with_heading, new String[]{"text","length"}, new int[]{R.id.textViewWithTxt, R.id.textViewWithNumberOfChars});
     }
 
     @NonNull
-    private List<Map<String, String>> prepareContent() {
-        List<Map<String, String>> contentList = new ArrayList<>();
-        String[] arrayContent = getString(R.string.large_text).split("\n\n");
+    private List<Map<String, String>> prepareAdapterContent(String[] stringsTxt) {
         Map<String, String> mapForList;
-        for (int i = 0 ; i < arrayContent.length ; i++){
+        for (String value: stringsTxt) {
             mapForList = new HashMap<>();
-            mapForList.put("text",arrayContent[i]);
-            mapForList.put("length",Integer.toString(arrayContent[i].length()));
+            mapForList.put("text",value);
+            mapForList.put("length",Integer.toString(value.length()));
             contentList.add(mapForList);
         };
         return contentList;
+    }
+
+    private String[] prepareCotent(){
+        String[] arrayContent = TxtSharedPref.getString(LARGE_TEXT, "").split("\n\n");
+        return arrayContent;
+    }
+
+    private void initList() {
+        ListView list = findViewById(R.id.list);
+        listContentAdapter = createAdapter(prepareCotent());
+        list.setAdapter(listContentAdapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                contentList.remove(position);
+                listContentAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    private void initSwipeToRefresh (){
+        swipeLayout = findViewById(R.id.swiperefresh);
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                updateList();
+                swipeLayout.setRefreshing(false);
+            }
+        });
+    }
+
+    private void updateList(){
+        contentList.clear();
+        prepareAdapterContent(prepareCotent());
+        listContentAdapter.notifyDataSetChanged();
+    }
+
+    private void saveInSharedPreferences(){
+        TxtSharedPref = getSharedPreferences("LargeTxt", MODE_PRIVATE);
+        if (!TxtSharedPref.getString(LARGE_TEXT, "").equals(getString(R.string.large_text))){
+            SharedPreferences.Editor myEditor = TxtSharedPref.edit();
+            String LargeTxt = getString(R.string.large_text);
+            myEditor.putString(LARGE_TEXT, LargeTxt);
+            myEditor.apply();
+        }
     }
 }
