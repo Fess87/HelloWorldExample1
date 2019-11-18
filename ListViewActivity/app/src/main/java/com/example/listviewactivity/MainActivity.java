10a3f50b5710a9a3bedb3009ff9fe04c4d49fdc5
Andrey Savelyev <fess_87@mail.ru>
2019-11-18 08:07:57 +0300

    new app with listview and actions

---
+++ ListViewActivity/app/src/main/java/com/example/listviewactivity/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.listviewactivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ListView list = findViewById(R.id.list);
+
+        List<Map<String, String>> values = prepareContent();
+
+        BaseAdapter listContentAdapter = createAdapter(values);
+
+        list.setAdapter(listContentAdapter);
+    }
+
+    @NonNull
+    private SimpleAdapter createAdapter(List<Map<String, String>>  content) {
+        return new SimpleAdapter(this, content, R.layout.list_with_heading, new String[]{"text","length"}, new int[]{R.id.textViewWithTxt, R.id.textViewWithNumberOfChars});
+    }
+
+    @NonNull
+    private List<Map<String, String>> prepareContent() {
+        List<Map<String, String>> contentList = new ArrayList<>();
+        String[] arrayContent = getString(R.string.large_text).split("\n\n");
+        Map<String, String> mapForList;
+        for (int i = 0 ; i < arrayContent.length ; i++){
+            mapForList = new HashMap<>();
+            mapForList.put("text",arrayContent[i]);
+            mapForList.put("length",Integer.toString(arrayContent[i].length()));
+            contentList.add(mapForList);
+        };
+        return contentList;
+    }
+}
