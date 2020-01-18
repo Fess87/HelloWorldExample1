ffe3a7ac75d163491f63ae2b8b2cef08e0d8c38d
Andrey Savelyev <fess_87@mail.ru>
2020-01-18 21:43:37 +0300

    add App

---
+++ NotesSavelyev/app/src/main/java/com/example/notessavelyev/App.java
@@ -0,0 +1,26 @@
+package com.example.notessavelyev;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    private static PinRepository pinRepository;
+
+    private static NoteRepository noteRepository;
+
+    public static PinRepository getPinRepository() {
+        return pinRepository;
+    }
+
+    public static NoteRepository getNoteRepository() {
+        return noteRepository;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        pinRepository = new HashedPinRepository(this);
+        noteRepository = new FileNoteRepository(this);
+    }
+}
